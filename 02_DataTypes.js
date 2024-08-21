05ad6423c3ee5764b9a3968eac9c03eda1804a38
Jahid <hzahid905@gmail.com>
2024-08-21 14:56:02 +0600

    02. DataTypes.js

---
+++ 02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using NODEJS not Browser
+
+let name = "Jahid"
+let age = 27
+let isLoggedIn = false
+let state
+let temperature = null
+
+
+// string => ""
+// number => 2^53
+// bigint
+// boolean => true/false
+// null => this is a standalone value
+// undefined =>
+// symbol => used for uniqueness
+
+
+// object
+
+console.log(typeof "Jahid")
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null)
+// *** undefined is a type but null is an object
