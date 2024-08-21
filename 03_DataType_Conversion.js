afca88f409989f0a1a7f8a2a946f2b79997f0ea2
Jahid <hzahid905@gmail.com>
2024-08-22 00:46:07 +0600

    part 2

--- 03_DataType_Conversion.js
+++ 03_DataType_Conversion.js
@@ -1,4 +1,4 @@
-
+/*
 let score = "33"
 
 console.log(typeof score)
@@ -32,3 +32,59 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
 // 1 => true; 0 => false; 11125 => true
 // "" => false; "Jahid" => true
+*/
+
+/*-----------------part-2---------------*/
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1 = "Hello"
+// let str2 = " Jahid"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// -----------Increment(++) operator-------------
+/*
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+*/
+
+//--------------comparison in dataType-------------------//
+// console.log(2>1);
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2" > 1) // automatically convert to number
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+
+console.log(undefined == 0)
+console.log(undefined < 0)
+console.log(undefined > 0)
+
+console.log("2" ===2);
